48aac1b5b87be6449334f002ddcd39b93e203247
Ucenik <Ucenik@UCIONICA3-10>
2017-03-07 18:00:34 +0100

    proba

---
+++ Operatori/Operatori/MuzickaLinija.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatori
+{
+   public class MuzickaLinija
+    {
+        private int ukupanBrojPjesama;
+
+        public int UkupanBrojPjesama
+        {
+
+            get { return  ukupanBrojPjesama; }
+            set { ukupanBrojPjesama = value; }
+         }
+
+        private int BrPjesama
+        {
+
+            set
+            {
+                if (value > 0 && value<ukupanBrojPjesama)
+                {
+                    BrPjesama = value;
+                }
+            }
+        }
+
+        private int stanje;
+
+        public int Stanje
+        {
+            get { return stanje; }
+            set { stanje = value; }
+        }
+
+        private string[] pjesme;
+
+        public int TrenutnoStanjeMuzickeLinije
+        {
+            get { return stanje; }
+        }
+
+        public string[] Pjesme
+        {
+            get { return pjesme; }
+            set
+            {
+                if (value.Length<=100)
+                {
+                    pjesme = value;
+
+                }
+            }
+        }
+
+        public MuzickaLinija(string[] nizPjesmi)
+        {
+            BrPjesama = 0;
+            Stanje = StanjeEnum.Paly.Id;
+           
+        }
+
+        public MuzickaLinija(string[] nizPjesmi, int pocetnaPjesma, int pocetnoStanje)
+        {
+            Pjesme = nizPjesmi;
+            BrPjesama = pocetnaPjesma;
+            Stanje = pocetnoStanje;
+        }
+
+
+        public MuzickaLinija(string[] nizPjesmi, int pocetnaPjesma, StanjeEnum pocetnoStanje)
+        {
+            Pjesme = nizPjesmi;
+            BrPjesama = pocetnaPjesma;
+            Stanje = pocetnoStanje.Id;
+        }
+
+
+    }
+}
---
+++ Operatori/Operatori/StanjeEnum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatori
+{
+   public class StanjeEnum :BaseEnum
+    {
+        public static StanjeEnum Stop = new StanjeEnum(0,"Stop");
+        public static StanjeEnum Paly = new StanjeEnum(1, "Play");
+        public static StanjeEnum Pause = new StanjeEnum(2, "Pause");
+        public static StanjeEnum Next = new StanjeEnum(3, "Next");
+        public static StanjeEnum Previous = new StanjeEnum(4, "Previous");
+
+
+
+        public StanjeEnum(int id, string value) : base(id, value)
+        {
+        }
+    }
+
+    public class BaseEnum
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+
+        public BaseEnum(int id, string value)
+        {
+            Id = id;
+            Value = value;
+        }
+    }
+}
