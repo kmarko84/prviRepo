c36c938aea2a361e371e08bce831347330723e70
Ucenik <Ucenik@UCIONICA3-10>
2017-03-07 20:50:42 +0100

    dsf

---
+++ MyFirstForm/MyFirstForm/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace MyFirstForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pgBar = new System.Windows.Forms.ProgressBar();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnStart.Location = new System.Drawing.Point(12, 64);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.Location = new System.Drawing.Point(197, 64);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            this.btnStop.Enter += new System.EventHandler(this.btnStop_Click);
+            // 
+            // pgBar
+            // 
+            this.pgBar.Location = new System.Drawing.Point(12, 226);
+            this.pgBar.Name = "pgBar";
+            this.pgBar.Size = new System.Drawing.Size(260, 23);
+            this.pgBar.TabIndex = 2;
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Location = new System.Drawing.Point(132, 132);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(44, 13);
+            this.lblValue.TabIndex = 3;
+            this.lblValue.Text = "lblValue";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.pgBar);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ProgressBar pgBar;
+        private System.Windows.Forms.Label lblValue;
+    }
+}
+
---
+++ MyFirstForm/MyFirstForm/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Pozdrav. " +((Button)sender).Text + " dugme.", "Ovo je title");
+            pgBar.Value += 10;
+            if (pgBar.Value==100)
+            {
+                MessageBox.Show("Pozdrav. " + ((Button)sender).Text + " dugme.", "Ovo je title");
+
+            }
+            else
+            {
+                lblValue.Text = pgBar.Value+"";
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pozdrav. " + ((Button)sender).Text + " dugme.", "Ovo je title");
+        }
+        private void btnStop_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblValue.Text = "0";
+        }
+    }
+}
